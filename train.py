fc06578dc2a4630639e45c48b7687c0fc55d581a
AmitKJha28 <amitjha8146@gmail.com>
2022-11-16 22:26:02 +0530

    TRAIN

--- train.py
+++ train.py
@@ -23,8 +23,29 @@ test_dataset_name = "Tree_test"
 test_images_path = "test"
 test_json_annot_path = "test.json"
 
+cfg_save_path = "OD_cfg.pickle"
+
 # register_dataset
 register_coco_instances(name=train_dataset_name,metadata={},json_file=train_json_annot_path,image_root=train_images_path)
 
 register_coco_instances(name=test_dataset_name,metadata={},json_file=test_json_annot_path,image_root=test_images_path)
-plot_samples(dataset_name=train_dataset_name,n=2)
+
+# plot_samples(dataset_name=train_dataset_name,n=2)
+
+
+
+def main():
+    cfg = get_train_cfg(config_file_path,checkpoint_url,train_dataset_name,test_dataset_name,num_classes,device,output_dir)
+
+    with open(cfg_save_path,"wb") as f:
+        pickle.dump(cfg,f,protocol=pickle.HIGHEST_PROTOCOL)
+
+    os.makedirs(cfg.OUTPUT_DIR,exist_ok=True)
+
+    trainer = DefaultTrainer(cfg)
+    trainer.resume_or_load(resume=False)
+
+    trainer.train()
+
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -19,3 +19,22 @@ def plot_samples(dataset_name, n=1):
         plt.figure(figsize=(15,20))
         plt.imshow(v.get_image())
         plt.show()
+
+def get_train_cfg(config_file_path,checkpoint_url,train_dataset_name,test_dataset_name,num_classes,device,output_dir):
+    cfg = get_cfg()
+
+    cfg.merge_from_file(model_zoo.get_config_file(config_file_path))
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(checkpoint_url)
+    cfg.DATASETS.TRAIN = (train_dataset_name,)
+    cfg.DATASETS.TEST = (test_dataset_name,)
+
+    cfg.DATALOADER.NUM_WORKERS = 2
+    cfg.SOLVER.IMS_PER_BATCH = 0.00025
+    cfg.SOLVER.MAX_ITER = 1000
+    cfg.SOLVER.STEPS = []
+
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = num_classes
+    cfg.MODEL.DEVICE = device
+    cfg.OUTPUT_DIR = output_dir
+
+    return cfg
